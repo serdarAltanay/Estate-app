8a80ff0cd371dbc34f30bea63682379a113ce166
serdarAltanay <serdaraltanay@gmail.com>
2025-02-05 15:23:39 +0300

    single page modified

--- client/estate-app/src/pages/singlePage/SinglePage.jsx
+++ client/estate-app/src/pages/singlePage/SinglePage.jsx
@@ -12,41 +12,144 @@ function SinglePage() {
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <div className="info">
-            <Suspense fallback={<p>Loading post details...</p>}>
-              <Await resolve={post}>
-                {(post) => (
-                  <>
-                    <div className="top">
-                      <div className="post">
-                        <h1>{post.title}</h1>
-                        <div className="address">
-                          <i className="fa-solid fa-location-pin"></i>
-                          <span>{post.address}</span>
-                        </div>
-                        <div className="price">$ {post.price}</div>
-                      </div>
-                      <div className="user">
-                        <img
-                          src={currentUser.avatar ? `http://localhost:8000${currentUser.avatar}` : "/noavatar.jpg"}
-                          alt="User Avatar"
-                        />
-                        <span>{post.user.username}</span>
-                      </div>
+          
+          {/* <Slider images={post.images} /> */}
+          <Suspense fallback={<p>Loading post details...</p>}>
+          <Await resolve={post}>
+            {(post) => (
+              <div className="info">
+                <div className="top">
+                  <div className="post">
+                    <h1>{post.title}</h1>
+                    <div className="address">
+                      <img src="/pin.png" alt="" />
+                      <span>{post.address}</span>
                     </div>
-                    <div className="features">
-                      <p className="title">Location</p>
-                      <div className="mapContainer">
-                        <Map items={[post]} />
-                      </div>
-                    </div>
-                  </>
-                )}
-              </Await>
-            </Suspense>
-          </div>
+                    <div className="price">$ {post.price}</div>
+                  </div>
+                  <div className="user">
+                  <img src={post.user.avatar ? `http://localhost:8000${post.user.avatar}` : "/noavatar.jpg"}  alt="" />
+                    <span>{post.user.username}</span>
+                  </div>
+                </div>
+                {/* <div
+                  className="bottom"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(post.postDetail.desc),
+                  }}
+                ></div> */}
+              </div>
+             )}
+            </Await>
+          </Suspense>
         </div>
       </div>
+      <Suspense fallback={<p>Loading post details...</p>}>
+          <Await resolve={post}>
+            {(post) => (
+              <div className="features">
+              <div className="wrapper">
+                <p className="title">General</p>
+                <div className="listVertical">
+                  <div className="feature">
+                    <img src="/utility.png" alt="" />
+                    <div className="featureText">
+                      <span>Utilities</span>
+                      {post.postDetail.utilities === "owner" ? (
+                        <p>Owner is responsible</p>
+                      ) : (
+                        <p>Tenant is responsible</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <img src="/pet.png" alt="" />
+                    <div className="featureText">
+                      <span>Pet Policy</span>
+                      {post.postDetail.pet === "allowed" ? (
+                        <p>Pets Allowed</p>
+                      ) : (
+                        <p>Pets not Allowed</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <img src="/fee.png" alt="" />
+                    <div className="featureText">
+                      <span>Income Policy</span>
+                      <p>{post.postDetail.income}</p>
+                    </div>
+                  </div>
+                </div>
+                <p className="title">Sizes</p>
+                <div className="sizes">
+                  <div className="size">
+                    <img src="/size.png" alt="" />
+                    <span>{post.postDetail.size} sqft</span>
+                  </div>
+                  <div className="size">
+                    <img src="/bed.png" alt="" />
+                    <span>{post.bedroom} beds</span>
+                  </div>
+                  <div className="size">
+                    <img src="/bath.png" alt="" />
+                    <span>{post.bathroom} bathroom</span>
+                  </div>
+                </div>
+                <p className="title">Nearby Places</p>
+                <div className="listHorizontal">
+                  <div className="feature">
+                    <img src="/school.png" alt="" />
+                    <div className="featureText">
+                      <span>School</span>
+                      <p>
+                        {post.postDetail.school > 999
+                          ? post.postDetail.school / 1000 + "km"
+                          : post.postDetail.school + "m"}{" "}
+                        away
+                      </p>
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <img src="/pet.png" alt="" />
+                    <div className="featureText">
+                      <span>Bus Stop</span>
+                      <p>{post.postDetail.bus}m away</p>
+                    </div>
+                  </div>
+                  <div className="feature">
+                    <img src="/fee.png" alt="" />
+                    <div className="featureText">
+                      <span>Restaurant</span>
+                      <p>{post.postDetail.restaurant}m away</p>
+                    </div>
+                  </div>
+                </div>
+                <p className="title">Location</p>
+                <div className="mapContainer">
+                  <Map items={[post]} />
+                </div>
+                <div className="buttons">
+                  <button>
+                    <img src="/chat.png" alt="" />
+                    Send a Message
+                  </button>
+                  {/* <button
+                    onClick={handleSave}
+                    style={{
+                      backgroundColor: saved ? "#fece51" : "white",
+                    }}
+                  >
+                    <img src="/save.png" alt="" />
+                    {saved ? "Place Saved" : "Save the Place"}
+                  </button> */}
+                </div>
+              </div>
+            </div>
+             )}
+            </Await>
+          </Suspense>
+      
     </div>
   );
 }
